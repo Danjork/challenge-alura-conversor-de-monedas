b6550c54ceda097056ad1a12e7c078bf963576ac
Danjork <40806603+Danjork@users.noreply.github.com>
2024-05-10 20:42:35 -0400

    consulta api

--- src/com/aluraChallenge/conversorDeMonedas/modelos/consultarApi.java
+++ src/com/aluraChallenge/conversorDeMonedas/modelos/consultarApi.java
@@ -2,6 +2,7 @@ package com.aluraChallenge.conversorDeMonedas.modelos;
 
 import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -14,6 +15,18 @@ public class consultarApi {
     HttpRequest request = HttpRequest.newBuilder()
             .uri(direccion)
             .build();
+
+    HttpResponse<String> response = null;
+
+    try{
+        response = client.send(request, HttpResponse.BodyHandler.ofString());
+
+    }catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+    {
+        return new Gson.fromJson(response.body(),Conversor.Class);
+    }
+
 
 
 }
